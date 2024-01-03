9d7c86f6136c939a954447ed9962f2ac42d62688
Hariskhangit2 <155529546+Hariskhangit2@users.noreply.github.com>
2024-01-03 14:51:20 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+//import React from "react";
+//import { createRoot } from "react-dom/client";
+
+//const heading = React.createElement('h1',{id: 'heading'},"hello from react");
+
+// const parent = React.createElement('div',
+// {id:'child'},
+// React.createElement('h1',{}, 'i am new newh1'))
+//const newroot = ReactDOM.createRoot(document.getElementById('root'))
+
+//const root = createRoot(document.getElementById('root'));
+
+//root.render(heading);
+
+console.log('hello')
+
